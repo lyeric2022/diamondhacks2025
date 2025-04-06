efdae04fad2f5724c4cae3aaffb6c01cf2111e63
Natasha <natashamarietran@gmail.com>
2025-04-06 00:00:40 -0700

    pushing navbar (finished) and semi complete page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import DisplayUser from './pages/DisplayUser';
 import './App.css';
+import HomePage from './pages/homePage';
 
 function App() {
   return (
-    <DisplayUser/>
+    <HomePage/>
   );
 }
 
